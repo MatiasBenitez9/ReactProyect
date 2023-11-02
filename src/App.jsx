d79c7a177e6c8803930169abcf9357f6bc47722c
Matias Benitez <msbenitez98@gmail.com>
2023-11-02 20:38:51 -0300

    1 commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+import Navrbar from './components/Navbar/Navrbar';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Navrbar/>
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Navbar/Navrbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Navbar.css'; 
+
+const Navrbar = () => {
+  return (
+    <div className="navbar-wrapper">
+      <nav className="navbar">
+        <div className="container">
+          <a href="/" className="navbar-brand">Mi Sitio</a>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a href="/" className="nav-link">Inicio</a>
+            </li>
+            <li className="nav-item">
+              <a href="/acerca" className="nav-link">Acerca de</a>
+            </li>
+            <li className="nav-item">
+              <a href="/contacto" className="nav-link">Contacto</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="categoriasDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Categorías
+              </a>
+              <div className="dropdown-menu" aria-labelledby="categoriasDropdown">
+                <a className="dropdown-item" href="/ropa-hombre">Ropa de Hombre</a>
+                <a className="dropdown-item" href="/ropa-mujer">Ropa de Mujer</a>
+                <a className="dropdown-item" href="/ropa-ninos">Ropa de Niños</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  )
+}
+export default Navrbar
