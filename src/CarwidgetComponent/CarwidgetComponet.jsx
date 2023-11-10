b250e40d2c2fe64d225cb8990a21433f66b5228a
Matias Benitez <msbenitez98@gmail.com>
2023-11-10 13:10:34 -0300

    Carwidget

---
+++ src/CarwidgetComponent/CarwidgetComponet.jsx
@@ -0,0 +1,11 @@
+ const CarwidgetComponent = () => {
+    return {
+        <div>
+            <span>Icono</span>
+            <span>10</span>       
+        </div>
+    }
+
+ }
+
+ export default CarwidgetComponent;
