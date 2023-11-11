a91b09c2dc0b0e0051df44814e748cc5a5cbe1cb
Matias Benitez <msbenitez98@gmail.com>
2023-11-11 13:20:18 -0300

    se modifico el ItemListContainer

--- src/ItemListContainer/ItemListContainer.jsx
+++ src/ItemListContainer/ItemListContainer.jsx
@@ -1,14 +1,13 @@
 // eslint-disable-next-line react/prop-types
 const ItemListContainer = ( {greeting} ) => {
-    // eslint-disable-next-line no-unused-vars
     const greetingStyles = {
         fontSize:'3rem',
         fontWeigth:'700',
         textAlign: 'center',
-        color:'#f4f4f4'
+        color:'#283747'
     }
     return(
-        <div>
+        <div style={greetingStyles}>
             {greeting}
         </div>
     )
